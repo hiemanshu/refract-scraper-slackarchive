698bd06781be6c7104e3df7be82005c9825d1bbb
Hiemanshu Sharma <hiemanshu@gmail.com>
2017-03-25 03:21:58 +0530

    Add method to parse team and get team id

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+const request = require('request-promise')
+
+const teamsApiPath = "http://api.slackarchive.io/v1/team"
+
+let validateConfig = (config) => {
+  if (config == undefined) {
+    throw "Undefined Config"
+  }
+  if (!("teamName" in config)) {
+    throw "Team Name not defined in config"
+  }
+  if (!("channels" in config)) {
+    throw "Channels not defined in config"
+  }
+}
+
+let getTeamId = (config) => {
+  const options = {
+    method: "GET",
+    uri: teamsApiPath
+  }
+
+  return new Promise((fulfill, reject) => {
+    request(options)
+      .then((response) => {
+        let parsedResponse = JSON.parse(response)
+        parsedResponse.team.forEach((item) => {
+          if (item.name === config.teamName) {
+            fulfill(item.team_id)
+          }
+        })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
 module.exports = (config, videoModel) => {
+  validateConfig(config)
+
+  getTeamId(config)
+    .then((teamId) => {
+      console.log(teamId)
+    })
 }
