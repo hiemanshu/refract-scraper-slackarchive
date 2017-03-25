135da36175891e311a7f90e806fba4c33e7c0b3c
Hiemanshu Sharma <hiemanshu@gmail.com>
2017-03-25 13:42:30 +0530

    Add method to get and parse messages

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const request = require('request-promise')
 const Joi = require('joi')
 
-const teamsApiPath = "http://api.slackarchive.io/v1/team"
 const channelsApiPath = "http://api.slackarchive.io/v1/channels"
+const messagesApiPath = "http://api.slackarchive.io/v1/messages"
+const teamsApiPath = "http://api.slackarchive.io/v1/team"
+
+const urlMatchRegex = /((www\.)?youtube\.com\/watch\?v=.{11}|(www\.)?youtu\.be\/.{11})/i
 
 let validateConfig = (config) => {
   const schema = Joi.object().keys({
@@ -51,7 +54,7 @@ let getChannels = (teamId, config) => {
   return new Promise((fulfill, reject) => {
     request(options)
       .then((response) => {
-        let channelIds = new Array()
+        let channelIds = new Array
         response.channels.forEach((channel) => {
           if (config.channels.includes(channel.name)) {
             channelIds.push(channel.channel_id)
@@ -65,14 +68,59 @@ let getChannels = (teamId, config) => {
   })
 }
 
+let parseMessages = (messages) => {
+  let videoUrls = new Array
+
+  messages.messages.forEach((message) => {
+    const match = message.text.match(urlMatchRegex)
+
+    if (match === null) {
+      return
+    } else {
+      videoUrls.push(match[0])
+    }
+  })
+
+  return videoUrls
+}
+
+let getMessagesForChannel = (channelId, timestamp) => {
+  const options = {
+    method: "GET",
+    uri: messagesApiPath,
+    qs: {channel: channelId, to: timestamp},
+    json: true
+  }
+
+  return new Promise((fulfill, reject) => {
+    request(options)
+      .then((response) => {
+        fulfill(response)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+let getMessages = (channelIds) => {
+  channelIds.forEach((channelId) => {
+    getMessagesForChannel(channelId)
+      .then((messages) => {
+        console.log(parseMessages(messages))
+      })
+  })
+}
+
 module.exports = (config, videoModel) => {
   validateConfig(config)
 
   getTeamId(config)
     .then((teamId) => {
-      getChannels(teamId, config)
-        .then((channelIds) => {
-          console.log(channelIds)
-        })
+      return getChannels(teamId, config)
+    })
+    .then((channelIds) => {
+      console.log(channelIds)
+      getMessages(channelIds)
     })
 }
