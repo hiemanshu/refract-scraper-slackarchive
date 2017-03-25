5fdeea2c6576e27c6205355c57b56ad680e162bd
Hiemanshu Sharma <hiemanshu@gmail.com>
2017-03-25 11:25:50 +0530

    Add method to parse channels and get channel ids

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const request = require('request-promise')
 const Joi = require('joi')
 
 const teamsApiPath = "http://api.slackarchive.io/v1/team"
+const channelsApiPath = "http://api.slackarchive.io/v1/channels"
 
 let validateConfig = (config) => {
   const schema = Joi.object().keys({
     teamName: Joi.string().required(),
-    channels: Joi.array().items(Joi.string().regex(/#.+/)).required()
+    channels: Joi.array().required()
   })
 
   const result = Joi.validate(config, schema)
@@ -20,18 +21,43 @@ let validateConfig = (config) => {
 let getTeamId = (config) => {
   const options = {
     method: "GET",
-    uri: teamsApiPath
+    uri: teamsApiPath,
+    json: true
   }
 
   return new Promise((fulfill, reject) => {
     request(options)
       .then((response) => {
-        let parsedResponse = JSON.parse(response)
-        parsedResponse.team.forEach((item) => {
+        response.team.forEach((item) => {
           if (item.name === config.teamName) {
             fulfill(item.team_id)
           }
         })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+let getChannels = (teamId, config) => {
+  const options = {
+    method: "GET",
+    uri: channelsApiPath,
+    qs: {team_id: teamId},
+    json: true
+  }
+
+  return new Promise((fulfill, reject) => {
+    request(options)
+      .then((response) => {
+        let channelIds = new Array()
+        response.channels.forEach((channel) => {
+          if (config.channels.includes(channel.name)) {
+            channelIds.push(channel.channel_id)
+          }
+        })
+        fulfill(channelIds)
       })
       .catch((err) => {
         reject(err)
@@ -44,6 +70,9 @@ module.exports = (config, videoModel) => {
 
   getTeamId(config)
     .then((teamId) => {
-      console.log(teamId)
+      getChannels(teamId, config)
+        .then((channelIds) => {
+          console.log(channelIds)
+        })
     })
 }
